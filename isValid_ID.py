6dabad8b3933130ef2aef87465bd6e36c73e5655
Atalay Samet Ergen <atalay.ergen@std.antalya.edu.tr>
2017-06-19 11:00:16 +0200

    Update isValid_ID.py

--- isValid_ID.py
+++ isValid_ID.py
@@ -1,10 +1,10 @@
 def isValid_TC_ID(value):
     value = str(value)
-    if (len(value) == 11 and value.isdigit() and not int(value[0]) == 0):
+    if (len(value) == 11 and value.isdigit() and not int(value[0]) == 0): #11 hanelidir, sadece rakamlardan oluşur, ilk hane 0 olamaz
         digits = [int(d) for d in str(value)]
         if (sum(digits[:10]) % 10 == digits[10]):
             if (((((7 * sum(digits[:9][-1::-2])) + (9 * sum(digits[:9][-2::-2]))) % 10) == digits[9]) and (
-                        ((8 * sum(digits[:9][-1::-2])) % 10) == digits[10])):
+                        ((8 * sum(digits[:9][-1::-2])) % 10) == digits[10])): #Kuralları README.md'de bulabilirsiniz
                 return True
 
     return False
